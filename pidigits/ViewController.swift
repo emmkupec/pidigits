24a04496ea09d8f69fa35e1c7843888afebd641e
emmkupec <32528910+emmkupec@users.noreply.github.com>
2017-11-20 12:24:51 -0500

    digits of pi!

--- pidigits/ViewController.swift
+++ pidigits/ViewController.swift
@@ -10,9 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var digitsCollection: UITextField!
+    
+    
+    @IBAction func checkAction(_ sender: Any) {
+        
+        
+        
+        
+        if digitsCollection.text == "" {
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter the digits of Pi!", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        } else {
+            if digitsCollection.text == "3.14159265358979323846264338327950288419716939937510582" {
+                let alertController = UIAlertController(title: "Good Job!", message: "You did it!", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "YAY!", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                present(alertController, animated: true, completion: nil)
+            
+            
+            
+            } else {
+                let alertController = UIAlertController(title: "Hmmm....", message: "We're not quite sure what happened, please try again!", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                present(alertController, animated: true, completion: nil)
+            
+            
+            }
+            }
+        
+        
+        
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+      
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
